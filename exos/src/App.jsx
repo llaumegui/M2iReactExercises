85a4e4c3d0bc1d7dcf3bec376f85173eb65c10bd
llaumegui <guillaume.lfbvre@gmail.com>
2025-03-07 10:09:58 +0100

    :sparkles: : add exo4

--- exos/src/App.jsx
+++ exos/src/App.jsx
@@ -1,20 +1,28 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import PersonTable from './components/exo1/personTable';
+import PersonTable from "./components/exo1/personTable";
 import Listing from "./components/exo2/listing";
 import FizzBuzz from "./components/exo3/fizzbuzz";
+import RealTimeName from "./components/exo4/realTimeName";
 
 function App() {
   return (
     <>
+      {/* Exo 1*/}
       <h1>Répertoire</h1>
-      <PersonTable/>
-      <hr/>
+      <PersonTable />
+      {/* Exo 2*/}
+      <hr />
       <h1>Listing</h1>
-      <Listing/>
-      <hr/>
+      <Listing />
+      <hr />
+      {/* Exo 3*/}
       <h1>FizzBuzz</h1>
-      <FizzBuzz/>
+      <FizzBuzz />
+      <hr />
+      {/* Exo 4*/}
+      <h1>RealTimeName</h1>
+      <RealTimeName />
     </>
   );
 }
---
+++ exos/src/components/exo4/realTimeName.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const RealTimeName = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const changeFirstName = (e) => setFirstName(e.target.value);
+  const changeLastName = (e) => setLastName(e.target.value.toUpperCase());
+
+  return (
+    <>
+      <input
+        className="firstName"
+        type="text"
+        placeholder="Prénom"
+        value={firstName}
+        onInput={changeFirstName}
+      />
+      <input
+        className="lastName"
+        type="text"
+        placeholder="Nom"
+        value={lastName}
+        onInput={changeLastName}
+      />
+      
+      <pre>
+        Bonjour{" "}<b>{firstName} {lastName}</b>!
+        Bienvenue sur l'application!
+      </pre>
+    </>
+  );
+};
+export default RealTimeName;
